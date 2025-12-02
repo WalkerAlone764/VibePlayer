cde5e80710658f68f4e087aeec335a05a9deeddc
Jatin <jatinsinghal@gmail.com>
2025-12-02 22:13:07 +0530

    setup colorscheme and typography handle status and navigation bar

--- app/src/main/java/com/upsidedowndev/vibeplayer/MainActivity.kt
+++ app/src/main/java/com/upsidedowndev/vibeplayer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.upsidedowndev.vibeplayer
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,7 +19,14 @@ import com.upsidedowndev.vibeplayer.ui.theme.VibePlayerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                scrim = Color.TRANSPARENT,
+            ),
+            navigationBarStyle = SystemBarStyle.dark(
+                scrim = Color.TRANSPARENT,
+            )
+        )
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             this.window.isNavigationBarContrastEnforced = false
         }
